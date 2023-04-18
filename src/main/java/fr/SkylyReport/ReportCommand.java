f465cbca7feeeea6f733edb2d48b9f058fe35557
Kisakay <53227802+Kisakay@users.noreply.github.com>
2023-04-18 21:03:32 +0200

    added functionnal and some bug are fixed

--- src/main/java/fr/SkylyReport/ReportCommand.java
+++ src/main/java/fr/SkylyReport/ReportCommand.java
@@ -4,73 +4,77 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import java.awt.Color;
+import java.io.File;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.bukkit.Bukkit;
-
+import java.util.Arrays;
+import org.bukkit.plugin.Plugin;
 public class ReportCommand implements CommandExecutor {
-    public static String globalPrefix = ChatColor.GOLD + "[SkyLy]";
+
+    Plugin SkylyReport = Bukkit.getPluginManager().getPlugin("SkylyReport");
+    File configFile = new File(SkylyReport.getDataFolder(), "config.yml");
+    FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+    public String globalPrefix = ChatColor.GOLD + config.getString("globalprefix");
+
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage(globalPrefix + ChatColor.WHITE + " This command can only be executed by a player");
+            sender.sendMessage(globalPrefix + ChatColor.WHITE + " " +config.getString("lang-console-cant-execute-command"));
             return true;
         }
         switch (args.length){
             case 0:
-                sender.sendMessage(globalPrefix + ChatColor.WHITE + " Entre le noms du joueur que tu veux signaler");
+                sender.sendMessage(globalPrefix + ChatColor.WHITE + " "+config.getString("lang-dont-type-player-command"));
                 return true;
             case 1:
-                sender.sendMessage(globalPrefix + ChatColor.WHITE + " Entre la raison");
+                sender.sendMessage(globalPrefix + ChatColor.WHITE + " "+config.getString("lang-dont-type-reason-command"));
                 return true;
             case 2:
                 break;
-            default:
-                sender.sendMessage(globalPrefix + ChatColor.WHITE + " La syntaxe de ta commande est incompréhensible");
-                return true;
         }
 
         String arg = args[0];
-        String arg2 = args[1];
+        String arg2 = String.join(" ", Arrays.copyOfRange(args, 1, args.length));
 
         Player reportedPlayer = Bukkit.getServer().getPlayer(arg);
 
         if (reportedPlayer == null) {
-            sender.sendMessage(globalPrefix + ChatColor.WHITE +" Le joueur spécifié est introuvable");
+            sender.sendMessage(globalPrefix + ChatColor.WHITE +" "+config.getString("lang-dont-found-player-command"));
             return true;
         }
 
         Player player = (Player) sender;
         if(!arg.isEmpty() && !arg2.isEmpty()){
-                   try {
-                       SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy HH:mm");
-                       Date date = new Date();
-                       String formattedDate = formatter.format(date);
+            SimpleDateFormat formatter = new SimpleDateFormat(config.getString("dateformat"));
+            Date date = new Date();
+            String formattedDate = formatter.format(date);
 
-            DiscordWebhook webhook = new DiscordWebhook("https://discord.com/api/webhooks/1097635034334249061/LX7HbPpv1sQhLpPyzotRMArDDK40_MiTbIVLVmhcqACHwP0Npmnvgl2G48d-smlUMpnM");
+            try {
+            DiscordWebhook webhook = new DiscordWebhook(config.getString("webhook"));
             webhook.setContent(":mega: || @  here ||");
             webhook.setAvatarUrl("https://media.discordapp.net/attachments/1073309804493287585/1097644114943684728/Logo_S.png");
             webhook.setUsername("SkyLy");
             webhook.setTts(false);
             webhook.addEmbed(new DiscordWebhook.EmbedObject()
-                    .setTitle("Report de "+player.getName().toString())
-                            .setDescription("Le report de **"+URLEncoder.encode(player.getName().toString(), "UTF-8")+"** qui signale le joueur **"+arg+"** a `"+formattedDate.toString()+"`"+ "\\nPour la raison :\\n**"+arg2+"**")
-                            .setColor(Color.RED)
+                    .setTitle("Report de "+player.getName())
+                    .setDescription("Le report de **"+URLEncoder.encode(player.getName(), "UTF-8")+"** qui signale le joueur **"+arg+"** a `"+formattedDate+"`"+ "\\nPour la raison :\\n**"+arg2+"**")
+                    .setColor(Color.RED)
                     .setThumbnail("https://mc-heads.net/avatar/"+player.getName())
                     .setFooter("Latest skyly.fr report detected - The server is currently on", "https://media.discordapp.net/attachments/1073309804493287585/1097644114943684728/Logo_S.png"));
-                webhook.execute(); //Handle exception
+                webhook.execute();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
 
-            //String command = "**[** 📣 **]** __Report__:      L'utilisateur **"+player.getName()+"** à envoyer un signalement à **"+arg+"** pour `"+arg2+"`";
-
-            player.sendMessage(globalPrefix + ChatColor.BLUE + " You have succefully reported this user lol.");
+            player.sendMessage(globalPrefix + ChatColor.WHITE + " "+config.getString("lang-succes-work-command"));
         }
         return true;
     }
--- src/main/java/fr/SkylyReport/SkylyReport.java
+++ src/main/java/fr/SkylyReport/SkylyReport.java
@@ -1,16 +1,48 @@
 package fr.SkylyReport;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.*;
+import org.bukkit.command.*;
+import org.bukkit.event.*;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SkylyReport extends JavaPlugin {
+
     @Override
     public void onEnable() {
+        File configFile = new File(getDataFolder(), "config.yml");
 
-        //Register your commands one by one and provide an instance of the class
+        if(!configFile.exists()){
+            getDataFolder().mkdirs();
+            try {
+                InputStream inputStream = getClass().getResourceAsStream("/config.yml");
+                Files.copy(inputStream, configFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+                getLogger().info("Le fichier config.yml a été extrait avec succès !");
+            } catch (IOException e) {
+                getLogger().warning("Impossible d'extraire le fichier config.yml !");
+                e.printStackTrace();
+            }
+        } else {
+            FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+
+            String maValeur = config.getString("ma-clef");
+        }
+
+        System.out.println("[SkyLyReport] "+"Dev by Kisakay aka PeacefulTrees x Ezermoz");
         getCommand("report").setExecutor(new ReportCommand());
     }
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
